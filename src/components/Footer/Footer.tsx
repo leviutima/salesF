fe2132ec3b71542644fc12fba463320aece700b9
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-05-15 22:10:50 -0300

    esperanca

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,44 +11,44 @@ import {
     Logo,
 } from "./Footer.style";
 
-
 const Footer = () => {
     return (
-        // <ClickerColector>
-            <Box>
-                <Logo>
-                    <img src="src\assets\logo-salesforce.svg" alt="" />
-                </Logo>
-                <FooterContainer>
-                    <Row>
-                        <Column>
-                            <Heading>Sobre nós</Heading>
-                            <FooterLink href="/Salesforce">
-                                O que é a Salesforce?
-                            </FooterLink>
-                            <FooterLink href="/crm">
-                                O que é CRM?
-                            </FooterLink>
-                        </Column>
-                        <Column>
-                            <Heading>Produtos</Heading>
-                            <FooterLink href="/salescloud">
-                                Sales Cloud
-                            </FooterLink>
-                            <FooterLink href="/einstein-gpt">
-                                Einstein GPT
-                            </FooterLink>
-                        </Column>
-                        <Column>
-                            <Heading>Contato</Heading>
-                            <FooterLink href="/contato">
-                                Fale conosco
-                            </FooterLink>
-                        </Column>
-                    </Row>
-                </FooterContainer>
-            </Box>
-        // </ClickerColector>
+        /* <ClickerColector> */
+        <Box>
+            <Logo>
+                <img src="/src/assets/logo-salesforce.svg" alt="" />
+            </Logo>
+            <FooterContainer>
+                <Row>
+                    <Column>
+                        <Heading>Sobre nós</Heading>
+                        <FooterLink href="/Salesforce">
+                            O que é a Salesforce?
+                        </FooterLink>
+                        <FooterLink href="/crm">
+                            O que é CRM?
+                        </FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Produtos</Heading>
+                        <FooterLink href="/salescloud">
+                            Sales Cloud
+                        </FooterLink>
+                        <FooterLink href="/einstein-gpt">
+                            Einstein GPT
+                        </FooterLink>
+                    </Column>
+                    <Column>
+                        <Heading>Contato</Heading>
+                        <FooterLink href="/contato">
+                            Fale conosco
+                        </FooterLink>
+                    </Column>
+                </Row>
+            </FooterContainer>
+        </Box>
+        /* </ClickerColector> */
     );
 };
+
 export default Footer;
