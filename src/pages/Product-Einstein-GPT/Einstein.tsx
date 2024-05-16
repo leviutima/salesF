f1703441304a2c5b3e2b955b3951cc4834deda19
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-05-15 22:48:18 -0300

    importando imagens do einstein

--- src/pages/Product-Einstein-GPT/Einstein.tsx
+++ src/pages/Product-Einstein-GPT/Einstein.tsx
@@ -1,5 +1,7 @@
 import { Layout } from "../../components/Layout/Layout"
 import {MainPageTitle, SectionConfig, SectionGridImgRight } from "../../components/Sections/Sections.style"
+import image8 from '../../assets/images/image 8.png'
+import image9 from '../../assets/images/image 9.png'
 
 function Einstein() {
     return(
@@ -10,7 +12,7 @@ function Einstein() {
                             <h1>Einstein GPT</h1>
                             <p>Um dos maiores diferenciais do Einstein GPT é a habilidade de oferecer experiências personalizadas. Ao analisar padrões de comportamento e interações passadas, a ferramenta pode prever e sugerir ações específicas para atender às necessidades individuais de cada cliente Isso cria um vínculo mais forte entre a marca e o cliente, resultando em maior fidelidade e satisfação.</p>
                         </SectionConfig>
-                            <img src="src\assets\images\image 8.png"/>
+                            <img src={image8}/>
                     </MainPageTitle>
                     <SectionGridImgRight>
                         <SectionConfig>
@@ -24,7 +26,7 @@ function Einstein() {
                                 <p>A automação é a chave para aprimorar a produtividade, e o Einstein GPT faz isso de forma excepcional. Ao automatizar tarefas repetitivas e fornecer insights acionáveis, permite que os profissionais de vendas e marketing dediquem mais tempo a atividades estratégicas e menos a processos operacionais. Esse é um movimento essencial para as empresas que querem crescem com foco, uma vez que se traduz em maior eficiência operacional e melhores resultados financeiros.</p>
                             </div>
                        </SectionConfig>
-                       <img src="src\assets\images\image 9.png" /> 
+                       <img src={image9}/> 
                      </SectionGridImgRight>
             </Layout>
         </>
