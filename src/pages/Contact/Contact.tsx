fe3b018bd5ca7a4fa09d19474f1b8b3053533221
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-05-19 22:54:05 -0300

    tirando react sem uso

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 import { Button } from "../../components/Button/Button"
 import { Layout } from "../../components/Layout/Layout"
 import { BackgroundForm, FormSection, Input, InputBox, ParagraphForm } from "./Contact.style"
